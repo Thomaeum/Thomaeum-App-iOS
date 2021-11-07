63ec72d9ad5140df8597d6c004fbe2f8bc7542bd
Simon Sure <info@simonsure.com>
2021-11-07 21:03:11 +0100

    Initial Commit

---
+++ Thomaeum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thomaeum
+//
+//  Created by Simon Sure on 07.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thomaeum/ThomaeumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThomaeumApp.swift
+//  Thomaeum
+//
+//  Created by Simon Sure on 07.11.21.
+//
+
+import SwiftUI
+
+@main
+struct ThomaeumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
